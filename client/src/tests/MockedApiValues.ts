493ba8509cd854a14acba35d024623463caf9f91
Jakub <jakub.lorencu@gmail.com>
2023-10-30 22:51:27 +0100

    fix test files

--- client/src/tests/MockedApiValues.ts
+++ client/src/tests/MockedApiValues.ts
@@ -1,39 +1,40 @@
 import React from "react"
 import { ICard, ISources } from "../base/utils/Axios/types"
+import { ECardTypes, EUnits } from "../base/utils/Axios/types"
 
 
 export const TEST_CARD_WALL: ICard = {
-    unit: "bricks",
+    unit: EUnits.BRICKS,
     price: 1,
     item_name: "wall",
-    type: "building",
+    type: ECardTypes.BUILDING,
     message: "fence +3",
     cardName: "Wall"
 }
 
 export const TEST_CARD_TOWER: ICard = {
-    unit: "bricks",
+    unit: EUnits.BRICKS,
     price: 1,
     item_name: "tower",
-    type: "building",
+    type: ECardTypes.BUILDING,
     message: "castle +5",
     cardName: "Tower"
 }
 
 export const TEST_CARD_ARCHER: ICard = {
-    unit: "weapons",
+    unit: EUnits.WEAPONS,
     price: 1,
     item_name: "archer",
-    type: "soldiers",
+    type: ECardTypes.SOLDIERS,
     message: "attack +2",
     cardName: "Archer"
 }
 
 export const TEST_CARD_SORCERER: ICard = {
-    unit: "crystals",
+    unit: EUnits.WEAPONS,
     price: 8,
     item_name: "sorcerer",
-    type: "magic",
+    type: ECardTypes.MAGIC,
     message: "mages +1",
     cardName: "Sorcerer"
 }
